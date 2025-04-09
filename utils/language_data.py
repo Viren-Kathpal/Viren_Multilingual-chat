b142e9497c1f46b2de9bcc93155764b499b93c9b
Viren Kathpal <105204147+Viren-Kathpal@users.noreply.github.com>
2025-04-09 19:34:33 +0530

    Create language_data.py

---
+++ utils/language_data.py
@@ -0,0 +1,16 @@
+# utils/language_data.py
+
+SUPPORTED_LANGUAGES = [
+    "english", "french", "spanish",
+    "arabic", "russian", "german", "japanese"
+]
+
+LANG_CODE_MAP = {
+    "english": "en",
+    "french": "fr",
+    "spanish": "es",
+    "arabic": "ar",
+    "russian": "ru",
+    "german": "de",
+    "japanese": "ja"
+}
